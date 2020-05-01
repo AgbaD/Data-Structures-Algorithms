a12d06026ded6691e36d5ae523eca24727bb6def
BlankGodd <blankgodd33@gmail.com>
2020-05-01 17:47:53 +0100

    Project Euler+

---
+++ Project Euler+/001.py
@@ -0,0 +1,19 @@
+#! /usr/bin/python
+# Author:	@BlankGodd
+
+
+def s_m(n):
+	a = [i for i in range(1, n) if i%3==0 or i%5==0]
+	return sum(a)
+
+if __name__ == "__main__":
+	ans = []
+	t = int(input("Enter the number of test cases: \n"))
+	for i in range(t):
+		n = int(input("Enter the integer n: "))
+		ans.append(s_m(n))
+
+	for i in ans:
+		print(i)
+
+
---
+++ Project Euler+/003.py
@@ -0,0 +1,31 @@
+#! /usr/bin/python
+# Author:	@BlankGodd
+
+def is_prime(n):
+	# Dosen't cover negative values
+	if n <= 1:
+		return False
+	elif n == 2 or n == 3:
+		return True
+	else:
+		for x in range(2,(n//2)+1):
+			if n%x==0:
+				return False
+		return True
+	
+# max prime factor
+def m_pf(n):
+	a = [i for i in range(1, n+1) if n%i==0]
+	b = [i for i in a if is_prime(i)]
+	print(b)
+	return max(b)
+
+
+if __name__ == "__main__":
+	ans = []
+	t = int(input("Enter the number of test cases: \n"))
+	for i in range(t):
+		n = int(input("Enter the integer n: "))
+		ans.append(m_pf(n))
+	for i in ans:
+		print(i)
