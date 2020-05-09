36506f01eebf74bb417e6769606fe2b43c0f67e7
BlankGodd <blankgodd33@gmail.com>
2020-05-09 20:43:37 +0100

    Further development

--- Project Euler+/010.py
+++ Project Euler+/010.py
@@ -2,28 +2,30 @@
 # Author:	@BlankGodd
 
 from datetime import datetime
+import math
 
 def is_prime(n):
 	# Dosen't cover negative values
+	sqr = int(math.sqrt(n))
 	if n <= 1:
 		return False
 	elif n == 2 or n == 3:
 		return True
 	else:
-		for x in range(2,(n//2)+1):
+		for x in range(2,sqr):
 			if n%x==0:
 				return False
 		return True
 
-# first n primes
-def fnprime(n):
+# sum of primes below n
+def primebn(n):
 	a = [i for i in range(2, n) if is_prime(i)]
 	return sum(a)
 
 
 if __name__ == "__main__":
 	st = datetime.utcnow()
-	print(fnprime(2_000_000))
+	print(primebn(2_000_00))
 	ed = datetime.utcnow()
 	print(ed-st)
 
--- Sorting/merge_sort.py
+++ Sorting/merge_sort.py
@@ -1,3 +1,52 @@
 #!/usr/bin/python
 # Author:	@BlankGodd
 
+import sys
+
+def sort(seq, count):
+	length = len(seq)
+	if length > 1:
+		mid = length // 2
+		left = seq[:mid]
+		right = seq[mid:]
+		count = sort(left, count)
+		count = sort(right, count)
+
+		# left_index, right_index, index
+		li, ri, i = 0, 0, 0
+		while li < len(left) and ri < len(right):
+			if left[li] < right[ri]:
+				seq[i] = left[li]
+				li += 1
+			else:
+				seq[i] = right[ri]
+				ri += 1
+			i += 1
+
+		while li < len(left):
+			seq[i] = left[li]
+			li += 1
+			i += 1
+
+		while ri < len(right):
+			seq[i] = right[ri]
+			ri += 1
+			i += 1
+
+		count += 1
+
+	if len(seq) == length:
+		return
+	return count
+
+
+if __name__ == "__main__":
+	a = input("Enter list Seperated by commas: \n")
+	try:
+		seq = [int(i) for i in a.split(",")]
+	except:
+		print("Invalid entry")
+		sys.exit(2)
+	count = 0
+	sort(seq, count)
+	print(seq)
