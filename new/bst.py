0d779ec742ddb65170e42cea773c42ec0e14f570
Damilare Agba <damilareagba1@gmail.com>
2021-05-10 11:57:10 +0100

    education on data structures

--- new/bst.py
+++ new/bst.py
@@ -34,6 +34,38 @@ class Bst:
                 node.left_child = temp
 
     def get_min(self):
+        if self.root.left_child:
+            return self.get_minimum(self.root.left_child)
+        return self.root.value
+
+    def get_minimum(self, node):
+        if node.left_child:
+            return self.get_minimum(node.left_child)
+        return node.value
+
+    def get_max(self):
+        if self.root.right_child:
+            return self.get_maximum(self.root.right_child)
+        return self.root.value
+
+    def get_maximum(self, node):
+        if node.right_child:
+            return self.get_maximum(node.right_child)
+        return node.value
+
+    def search(self, value):    # to see if a value is in a bst
+        if value < self.root.value:
+            return self.search_left(value, self.root)
+        elif value > self.root.value:
+            return self.search_right(value, self.root)
+        else:
+            if value != self.root.value:
+                return False
+            return True
+    
+    def search_left(self, node):
+        if value == node.value:
+
 
 
     
