abcae0d1b40f9d183c34d4fb22350c7dc52345ff
BlankGodd <blankgodd33@gmail.com>
2021-08-03 00:08:46 +0100

    cruise

--- c#/linkedList/Program.cs
+++ c#/linkedList/Program.cs
@@ -7,7 +7,7 @@ namespace linkedList
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("We are Inside!");
             LinkedList lst = new LinkedList();
 
             // lst.traverse();
