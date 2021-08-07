2f88d4fd37200a4617e57a479b7e6ad96b26f538
BlankGodd <blankgodd33@gmail.com>
2021-08-07 23:52:36 +0100

    binary search tree implementation

--- c#/bst/Bst.cs
+++ c#/bst/Bst.cs
@@ -6,6 +6,7 @@ namespace bst
     class Bst
     {
         public Node root;
+        public List<int> tree;
 
         public Bst()
         {
@@ -13,9 +14,156 @@ namespace bst
         }
 
         // insert
-        // search
+        public void insert(int data)
+        {
+            Node temp = new Node(data);
+            if (root == null)
+            {
+                root = temp;
+            }
+            else
+            {
+                if (root.data < data)
+                {
+                    if (root.rightChild != null)
+                    {
+                        insertNode(root.rightChild, temp);
+                    }
+                    else
+                    {
+                        root.rightChild = temp;
+                    }
+                }
+                else if (root.data > data)
+                {
+                    if (root.leftChild != null)
+                    {
+                        insertNode(root.leftChild, temp);
+                    }
+                    else
+                    {
+                        root.leftChild = temp;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Value already present in binary search tree!");
+                }
+            }
+        }
+
+        public void insertNode(Node node, Node newnode)
+        {
+            if (newnode.data > node.data)
+            {
+                if (node.rightChild == null)
+                {
+                    node.rightChild = newnode;
+                }
+                else
+                {
+                    insertNode(node.rightChild, newnode);
+                }
+            }
+            else if (newnode.data < node.data)
+            {
+                if (node.leftChild == null)
+                {
+                    node.leftChild = newnode;
+                }
+                else
+                {
+                    insertNode(node.leftChild, newnode);
+                }
+            }
+            else {
+                Console.WriteLine("Value already present in binary search tree!");
+            }
+        }
+
         // get minimum
+        public int getMin()
+        {
+            if (root == null)
+            {
+                Console.WriteLine("Binary search tree is empty!");
+                return 0;
+            }
+            else
+            {
+                return getMinimun(root);
+            }
+        }
+
+        public int getMinimun(Node node)
+        {
+            if (node.leftChild != null)
+            {
+                return getMinimun(node.leftChild);
+            }
+            else
+            {
+                return node.data;
+            }
+        }
+
         // get maximum
+        public int getMax()
+        {
+            if (root == null)
+            {
+                Console.WriteLine("Binary search tree is empty!");
+                return 0;
+            }
+            else
+            {
+                return getMaximum(root);
+            }
+        }
+
+        public int getMaximum(Node node)
+        {
+            if (node.rightChild != null)
+            {
+                return getMaximum(node.rightChild);
+            }
+            else
+            {
+                return node.data;
+            }
+        }
         // traverse
+        public void traverse()
+        {
+            tree  = new List<int>();
+            if (root == null)
+            {
+                Console.WriteLine("Binary search tree is empty!");
+            }
+            else
+            {
+                traverseTree(root);
+            }
+            foreach (int item in tree)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public void traverseTree(Node node)
+        {
+            if (node.leftChild != null)
+            {
+                traverseTree(node.leftChild);
+            }
+
+            tree.Add(node.data);
+
+            if (node.rightChild != null)
+            {
+                traverseTree(node.rightChild);
+            }
+        }
+        // search
     }
 }
--- c#/bst/Program.cs
+++ c#/bst/Program.cs
@@ -7,6 +7,31 @@ namespace bst
         static void Main(string[] args)
         {
             Console.WriteLine("Binary Search Tree");
+
+            Bst bst = new Bst();
+
+            bst.traverse();
+            Console.WriteLine("");
+
+            bst.insert(3);
+            bst.insert(2);
+
+            
+            bst.traverse();
+            Console.WriteLine("");
+
+            bst.insert(2);
+            bst.insert(5);
+
+            bst.traverse();
+            Console.WriteLine("");
+
+            Console.WriteLine(bst.getMin());
+            Console.WriteLine("");
+            Console.WriteLine(bst.getMax());
+            Console.WriteLine("");
+
+            bst.traverse();
         }
     }
 }
