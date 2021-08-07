5e69b64c75728b92e7bd16835e413e762e30d2dc
BlankGodd <blankgodd33@gmail.com>
2021-08-07 23:04:36 +0100

    binary search tree

---
+++ c#/bst/Bst.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+
+namespace bst
+{
+    class Bst
+    {
+        public Node root;
+
+        public Bst()
+        {
+            root  = null;
+        }
+
+        // insert
+        // search
+        // get minimum
+        // get maximum
+        // traverse
+    }
+}
---
+++ c#/bst/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace bst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Binary Search Tree");
+        }
+    }
+}
