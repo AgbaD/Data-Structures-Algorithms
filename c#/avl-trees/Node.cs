7960e5c3444ecb4b9a8c5d88af12d8d3a20aa62f
BlankGodd <blankgodd33@gmail.com>
2021-08-09 01:04:32 +0100

    avl trees

---
+++ c#/avl-trees/Node.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace avl_trees
+{
+    class Node
+    {
+        
+    }
+}
---
+++ c#/avl-trees/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace avl_trees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Avl trees!");
+        }
+    }
+}
