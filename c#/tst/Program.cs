a64276ac029f48f9d606b97576c7c36583deb427
BlankGodd <blankgodd33@gmail.com>
2021-08-13 04:55:45 +0100

    it works (tst)

--- c#/tst/Program.cs
+++ c#/tst/Program.cs
@@ -7,7 +7,23 @@ namespace tst
         static void Main(string[] args)
         {
             Console.WriteLine("Tenary Search Tree!");
-            
+
+            Tst t = new Tst();
+            t.put("cat", 23);
+            t.put("apple", 46);
+            t.put("car", 6);
+            t.put("cow", 112);
+
+            int a = t.get("cow");
+            if (a == -1)
+            {
+                Console.WriteLine("Key error!");
+            }
+            else
+            {
+                Console.WriteLine(a);
+            }
+            // Console.WriteLine(t.root.character);
         }
     }
 }
--- c#/tst/Tst.cs
+++ c#/tst/Tst.cs
@@ -6,6 +6,7 @@ namespace tst
 {
     class Tst
     {
+        // negative values are not allowed
         public Node root;
         public Node pointer;
         public string alpha;
@@ -20,123 +21,180 @@ namespace tst
         // insertion
         public void put(string word, int value)
         {
-            pointer = root;
-            int ind = 0;
-            while (true)
+            if (root == null)
             {
-                if (pointer == null)
+                int ind = 0;
+                root = new Node(word[ind]);
+                pointer = root;
+                int last = word.Length - 1;
+                for (int i=ind+1; i < word.Length; i++)
                 {
-                    if (word.Length > ind+1)
+                    if (i == last)
                     {
-                        pointer = new Node(word[ind]);
-                        int last = word.Length - 1;
-                        for (int i=ind+1; i < word.Length; i++)
+                        Node temp = new Node(word[i]);
+                        temp.value = value;
+                        pointer.middle = temp;
+                    }
+                    else
+                    {
+                        Node temp = new Node(word[i]);
+                        pointer.middle = temp;
+                        pointer = pointer.middle;
+                    }
+                }
+            }
+            else
+            {
+                pointer = root;
+                int ind = 0;
+                while (true)
+                {
+                    int pointer_ind = alpha.IndexOf(pointer.character);
+                    int word_ind = alpha.IndexOf(word[ind]);
+
+                    if (word_ind < pointer_ind)
+                    {
+                        if (pointer.left == null)
                         {
-                            if (i == last)
+                            if (word.Length > ind+1)
                             {
-                                Node temp = new Node(word[i]);
-                                temp.value = value;
-                                pointer.middle = temp;
+                                pointer.left = new Node(word[ind]);
+                                pointer = pointer.left;
+                                int last = word.Length - 1;
+                                for (int i=ind+1; i < word.Length; i++)
+                                {
+                                    if (i == last)
+                                    {
+                                        Node temp = new Node(word[i]);
+                                        temp.value = value;
+                                        pointer.middle = temp;
+                                    }
+                                    else
+                                    {
+                                        Node temp = new Node(word[i]);
+                                        pointer.middle = temp;
+                                        pointer = pointer.middle;
+                                    }
+                                }
                             }
                             else
                             {
-                                Node temp = new Node(word[i]);
-                                pointer.middle = temp;
-                                pointer = pointer.middle;
+                                pointer.left = new Node(word[ind]);
+                                pointer.left.value = value;
                             }
-                        }
-                    }
-                    else
-                    {
-                        pointer = new Node(word[ind]);
-                        pointer.value = value;
-                    }
-                    break;
-                }
-
-                int pointer_ind = alpha.IndexOf(pointer.character);
-                int word_ind = alpha.IndexOf(word[ind]);
-
-                if (word_ind < pointer_ind)
-                {
-                    if (pointer.left == null)
-                    {
-                        if (word.Length > ind+1)
-                        {
-                            pointer.left = new Node(word[ind]);
-                            pointer = pointer.left;
-                            int last = word.Length - 1;
-                            for (int i=ind+1; i < word.Length; i++)
-                            {
-                                if (i == last)
-                                {
-                                    Node temp = new Node(word[i]);
-                                    temp.value = value;
-                                    pointer.middle = temp;
-                                }
-                                else
-                                {
-                                    Node temp = new Node(word[i]);
-                                    pointer.middle = temp;
-                                    pointer = pointer.middle;
-                                }
-                            }
+                            break;
                         }
                         else
                         {
-                            pointer.left = new Node(word[ind]);
-                            pointer.left.value = value;
+                            pointer = pointer.left;
                         }
-                        break;
                     }
-                    else
+                    else if (word_ind > pointer_ind)
+                    {
+                        if (pointer.right == null)
+                        {
+                            if (word.Length > ind+1)
+                            {
+                                pointer.right = new Node(word[ind]);
+                                pointer = pointer.right;
+                                int last = word.Length - 1;
+                                for (int i=ind+1; i < word.Length; i++)
+                                {
+                                    if (i == last)
+                                    {
+                                        Node temp = new Node(word[i]);
+                                        temp.value = value;
+                                        pointer.middle = temp;
+                                    }
+                                    else
+                                    {
+                                        Node temp = new Node(word[i]);
+                                        pointer.middle = temp;
+                                        pointer = pointer.middle;
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                pointer.right = new Node(word[ind]);
+                                pointer.right.value = value;
+                            }
+                            break;
+                        }
+                        else
+                        {
+                            pointer = pointer.right;
+                        }
+                    }
+                    else // word_ind == pointer_ind
+                    {
+                        pointer = pointer.middle;
+                        ind++;
+                    }
+                }
+            }
+        }
+
+        // Removal
+        public int get(string key)
+        {
+            
+            pointer = root;
+            int ind = 0;
+            int last = key.Length - 1;
+            while (true)
+            {
+                int pointer_ind = alpha.IndexOf(pointer.character);
+                int key_ind = alpha.IndexOf(key[ind]);
+
+                if (key_ind < pointer_ind)
+                {
+                    if (pointer.left != null)
                     {
                         pointer = pointer.left;
                     }
-                }
-                else if (word_ind > pointer_ind)
-                {
-                    if (pointer.right == null)
+                    else
                     {
-                        if (word.Length > ind+1)
-                        {
-                            pointer.right = new Node(word[ind]);
-                            pointer = pointer.right;
-                            int last = word.Length - 1;
-                            for (int i=ind+1; i < word.Length; i++)
-                            {
-                                if (i == last)
-                                {
-                                    Node temp = new Node(word[i]);
-                                    temp.value = value;
-                                    pointer.middle = temp;
-                                }
-                                else
-                                {
-                                    Node temp = new Node(word[i]);
-                                    pointer.middle = temp;
-                                    pointer = pointer.middle;
-                                }
-                            }
-                        }
-                        else
-                        {
-                            pointer.right = new Node(word[ind]);
-                            pointer.right.value = value;
-                        }
                         break;
                     }
-                    else
+                }
+                else if (key_ind > pointer_ind)
+                {
+                    if (pointer.right != null)
                     {
                         pointer = pointer.right;
                     }
+                    else
+                    {
+                        break;
+                    }
                 }
-                else // word_ind == pointer_ind
+                else // key_ind == pointer_ind
                 {
-                    pointer = pointer.middle;
-                    ind++;
+                    if (ind == last)
+                    {
+                        try
+                        {
+                            return pointer.value;
+                        }
+                        catch (System.Exception)
+                        {
+                            break;
+                            throw;
+                        }
+                    }
+                    if (pointer.middle != null)
+                    {
+                        pointer = pointer.middle;
+                        ind++;
+                    }
+                    else
+                    {
+                        break;
+                    }
                 }
             }
+            return -1;
         }
     }
 }
